291d4c4152937283ed9a37c1dc3925708757ecfb
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-04-18 07:35:52 +0800

    编写TitleFragment并修复了一个布局问题

--- app/src/main/java/com/winnerwinter/calculationtest/TitleFragment.java
+++ app/src/main/java/com/winnerwinter/calculationtest/TitleFragment.java
@@ -2,11 +2,20 @@ package com.winnerwinter.calculationtest;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.SavedStateViewModelFactory;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.winnerwinter.calculationtest.databinding.FragmentTitleBinding;
 
 
 /**
@@ -20,9 +29,25 @@ public class TitleFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        MyViewModel myViewModel;
+        // myViewModel = ViewModelProviders.of(requireActivity()).get(MyViewModel.class);  // api 弃用
+        myViewModel = new ViewModelProvider(requireActivity(), new SavedStateViewModelFactory(requireActivity().getApplication(), requireActivity())).get(MyViewModel.class);
+        FragmentTitleBinding binding;
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(requireActivity());
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController controller = Navigation.findNavController(view);
+                controller.navigate(R.id.action_titleFragment_to_questionFragment);
+            }
+        });
+        return binding.getRoot();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false);
+        //return inflater.inflate(R.layout.fragment_title, container, false);
     }
 }
